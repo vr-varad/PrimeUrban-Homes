87ade151e97782797f436bbd7f0ef65a59629dc5
vr-varad <varadgupta21#gmail.com>
2024-04-15 20:21:29 +0530

    final changes

--- api/index.js
+++ api/index.js
@@ -17,11 +17,11 @@ app.use('/api/user',userRoutes);
 app.use('/api/auth',authRoutes);
 app.use('/api/listing',listingRoutes);
 
-app.use(express.static(path.join(__dirname, '../client/build')))
+app.use(express.static(path.join(__dirname, 'client/dist')))
 
 
 app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '../client/build/index.html'))
+    res.sendFile(path.join(__dirname, 'client/dist/index.html'))
 })
 
 app.use((err,req,res,next)=>{
