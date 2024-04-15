db0d90e293eaf8737a35dca43fe3a09ec838c7b7
vr-varad <varadgupta21#gmail.com>
2024-04-15 19:57:53 +0530

    final commit

--- api/index.js
+++ api/index.js
@@ -23,7 +23,7 @@ app.use(express.static(path.join(__dirname, 'client/build')))
 
 app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, 'client/build/index.html'))
-}
+})
 
 app.use((err,req,res,next)=>{
     const statusCode = err.statusCode || 500;
